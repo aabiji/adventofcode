9b3ef433fb04bb9ef17c32bfb412fb3435675c63
Abigail Adegbiji <adegbijiabigail@gmail.com>
2022-11-06 16:53:24 -0800

    Finished day 12.

---
+++ day12.py
@@ -0,0 +1,14 @@
+inputs = open("day12-inputs.txt", "r").read().split("\n")
+
+# A component graph
+import networkx
+
+graph = networkx.Graph()
+for i in inputs:
+    node, neighbors = i.split(" <-> ")
+    graph.add_edges_from((node, neighbor) for neighbor in neighbors.split(", "))
+
+connected_to_zero = len(networkx.node_connected_component(graph, '0')) # Part 1: How many? Answer: 175
+connected_components = networkx.number_connected_components(graph) # Part 2: How many? 
+
+print(connected_to_zero, connected_components)
