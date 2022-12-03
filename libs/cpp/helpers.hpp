dbb0652890e8bea8626ff8656ad4c9639edca5ef
Abigail Adegbiji <adegbijiabigail@gmail.com>
2022-12-02 18:38:42 -0600

    Adding c++ helpers.

---
+++ libs/cpp/helpers.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <fstream>
+
+std::vector<std::string> splitPuzzleInputs(std::string inputs);
+std::string readPuzzleInputs(std::string filename);
